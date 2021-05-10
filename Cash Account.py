531fad3d35878c26afd8c893fcc5816ea1490757
jakesjacob <33692879+jakesjacob@users.noreply.github.com>
2021-05-10 21:50:30 +0100

    deposits

--- Cash Account.py
+++ Cash Account.py
@@ -1,12 +1,27 @@
 cashAccount = 0
 
 
-def depositCash(value):
+def depositCash(value, cashAccount):
     if value <= 500:
-        cashAccount = cashAccount + value
+        newCashAccount = cashAccount + value
     elif value > 500:
         print("The deposit limit is £500")
     elif value == 0:
         print("Please enter a value larger than 0")
     elif value < 0:
         print("Please enter a value larger than 0")
+    return newCashAccount
+
+
+thisIsTrue = True
+
+while thisIsTrue:
+    #cashAccount = 0
+    print("Your Cash Account value = " + str(cashAccount))
+    print(cashAccount)
+    print("Menu")
+    print("1 - Deposit")
+    if input("Press 1 for Deposits") == "1":
+        print("Enter the amount you want to deposit: ")
+        value = input("Amount = ")
+        cashAccount = depositCash(int(value), cashAccount)
